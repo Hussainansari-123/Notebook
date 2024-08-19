e274f91759ec03181029fe003f0edcfa85b4388b
Hussain <sarifansari660@gmail.com>
2024-08-19 13:20:24 +0530

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo=require('./db')
+connectToMongo()
+var cors = require('cors')
+const express = require('express')
+const app = express()
+ 
+
+
+const port = 5000
+app.use(express.json())
+
+app.use(cors())
+//Availabel Routes
+app.use('/api/auth', require('./routes/auth') )
+app.use('/api/notes', require('./routes/notes') )
+
+app.listen(port, () => {
+  console.log(`iNotebook backend is running at http://localhost:${port}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 
+import NoteState from "./Context/notes/NoteState";
+import Alert from "./Components/Alert";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { useState } from "react";
 function App() {
+const [alert,setAlert]=useState(null)
+const showalert=(message,type)=>{
+
+  setAlert({
+    msg:message,
+    type:type
+  })
+  setTimeout(()=>{
+setAlert(null)
+  },1000)
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+    <NoteState>
+      <Router>
+        <Navbar />
+        <Alert alert={alert}/>
+        <div className="container mt-5">
+        <Routes>
+          <Route exact path="/" element={<Home showalert={showalert}/>}/>
+        
+          <Route exact path="/login" element={<Login  showalert={showalert}/>}/>
+          <Route exact path="/signup" element={<Signup  showalert={showalert}/>}/>
+        </Routes>
+      
+          
+         
+        </div>
+      </Router>
+      </NoteState>
+
+    </>
   );
 }
 
